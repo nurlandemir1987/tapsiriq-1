f13303d83472215c18381f34e79d911deb667e2f
nurlandemir1987 <nurlannd@code.edu.az>
2022-12-11 15:35:22 +0400

    tapsiriq 1

---
+++ 1-ci tapsiriq/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _1_ci_tapsiriq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("edelerin cemini goster");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a}+{b}={a + b}");
+        }
+    }
+}
